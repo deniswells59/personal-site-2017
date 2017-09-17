037e3130efc5e418a60d0224c7aa48e9d19a1c65
Denis Wells <deniswells59@gmail.com>
2017-09-16 17:24:20 -0700

    icons

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -10,7 +10,6 @@ class Logo extends Component {
       halfDur : this.props.dur / 2,
       quarDur : this.props.dur / 4,
       delayDur: this.props.delay || 0,
-
       backgroundColor: this.props.backgroundColor || '#000',
     }
   }
@@ -22,17 +21,14 @@ class Logo extends Component {
            style={{backgroundColor: `${this.state.backgroundColor}`}}
            viewBox="0 0 100 100">
         <path id='D'
-          fill='none' stroke="#fcfcfc" strokeWidth="1" strokeMiterlimit="0"
-          d="M10,10 c60,3 60,70 0,73 L10,10" />
-        <path id='W-1'
-          fill='none' stroke="#fcfcfc" strokeWidth="1" strokeMiterlimit="0"
-          d="M25,15 L41,70" />
+          fill='none' stroke="#fcfcfc" strokeWidth="3" strokeMiterlimit="0"
+          d="M10,10 c60,3 60,70 0,73 L10,8" />
         <path id='W-2'
-          fill='none' stroke="#fcfcfc" strokeWidth="1" strokeMiterlimit="0"
-          d="M42.5,74.5 L45.3,83 L60,65 L74.7,83 L90,15" />
+          fill='none' stroke="#fcfcfc" strokeWidth="3" strokeMiterlimit="0"
+          d="M42.5,75.5 L45.3,83 L60,65 L74.7,83 L90,15" />
 
           <animate xlinkHref='#D' attributeName='stroke-dashoffset' id="animateD"
-            from='201' to='0' dur={`${this.state.halfDur}s`} begin={`${this.state.delayDur}s`} fill='freeze'
+            from='202.68' to='0' dur={`${this.state.halfDur}s`} begin={`${this.state.delayDur}s`} fill='freeze'
             keySplines="0 0.2 0.5 1; 0 0.2 0.5 1; 0 0.2 0.5 1; 0 0.2 0.5 1; 0 0.2 0.5 1; 0 0.2 0.5 1"
         		keyTimes="0;0.22;0.33;0.55;0.66;0.88;1" calcMode="spline"/>
 
--- src/components/Professional/index.js
+++ src/components/Professional/index.js
@@ -9,9 +9,14 @@ class Professional extends Component {
     return (
       <div className="Professional">
         <div className="header">
+
           <div className="info-wrapper" id="info">
             <Logo size="100" dur="2" delay=".5" backgroundColor="transparent" />
-
+            <div className="social-links">
+              <i className="icon-social-github icons"></i>
+              <i className="icon-social-linkedin icons"></i>
+              <i className="icon-social-twitter icons"></i>
+            </div>
           </div>
           <div className="layer-wrapper">
             <Parallax opacity='0.3' scrollOffset='0.15' size='20'/>
